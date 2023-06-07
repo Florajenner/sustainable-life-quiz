a5ac5f27076c58f1cade74bb61f1f7e9917372ed
FLORA JENNER <flora.jenner@gmail.com>
2023-06-07 14:37:18 +0000

    set up basic javascript variables for the quiz

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const quizData = [
+    // Quiz questions
+  ];
+  
+  const quizContainer = document.getElementById("quiz-container");
+  const questionContainer = document.getElementById("question-container");
+  const optionContainer = document.getElementById("option-container");
+  const submitButton = document.getElementById("submit-btn");
+  const resultContainer = document.querySelector("#result-container");
+  const restart = document.getElementById("again");
+ 
+  
+  let currentQuestion = 0;
+  let score = 0;
+  
